d86dc7e939ebab15385a6b0a8ab518d9239cc6d8
Nuria Silvi <Nuria.L.Silvi@gmail.com>
2025-09-08 23:41:10 -0300

    sync

--- clase-02/presentacion_discord.py
+++
@@ -1,9 +0,0 @@
-print("Presiona 1 para saber quién soy:")
-opcion = input()
-
-if opcion == "1":
-    nombre = "Nuria"
-    historia = "Empecé en tecnología hace mucho, hice un break largo en otros ámbitos, \
-y ahora encontré mi condición de parada: este curso."
-    print("Hola,", nombre)
-    print(historia)
